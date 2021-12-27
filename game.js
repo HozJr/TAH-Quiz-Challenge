1f9729071e840324972e47caef791c4a2bb06a23
HozJr <hozjr2006@gmail.com>
2021-12-26 18:15:49 -0600

    timer function

--- game.js
+++ game.js
@@ -37,3 +37,26 @@ var Time = document.querySelector("#Time");
 var timer = document.querySelector("#startTimer");
 var questionsDiv = document.querySelector("#questionsDiv");
 var mainbox = document.querySelector("#mainbox");
+
+// Timer Variables
+var secondsLeft = 76;
+var holdTimer = 0;
+var penalty = 10;
+var ulCreate = document.createElement("ul");
+
+// Triggers timer on button
+timer.addEventListener("click", function () {
+    if (holdTimer === 0) {
+        holdTimer = setInterval(function () {
+            secondsLeft--;
+            Time.textContent = "Time: " + secondsLeft;
+
+            if (secondsLeft <= 0) {
+                clearInterval(holdTimer);
+                allDone();
+                Time.textContent = "Time's up!";
+            }
+            }, 1000);
+    }
+    render(questionIndex);
+});
