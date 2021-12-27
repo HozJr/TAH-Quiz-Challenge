f603d9bdc895452e5972dfa281b5ee6122b66176
HozJr <hozjr2006@gmail.com>
2021-12-26 18:01:22 -0600

    Questions and variables

---
+++ game.js
@@ -0,0 +1,39 @@
+var questions = [
+    {
+        title: "question 1:",
+        choices: ["1", "2", "3", "4"],
+        answer: ""
+    },
+
+    {
+        title: "question 2:",
+        choices: ["1", "2", "3", "4"],
+        answer: ""
+    },
+
+    {
+        title: "question 3:",
+        choices: ["1", "2", "3", "4"],
+        answer: ""
+    },
+
+    {
+        title: "question 4:",
+        choices: ["1", "2", "3", "4"],
+        answer: ""
+    },
+
+    {
+        title: "question 5:",
+        choices: ["1", "2", "3", "4"],
+        answer: ""
+    },
+];
+
+// Variables
+var score = 0;
+var questionIndex = 0;
+var Time = document.querySelector("#Time");
+var timer = document.querySelector("#startTimer");
+var questionsDiv = document.querySelector("#questionsDiv");
+var mainbox = document.querySelector("#mainbox");
